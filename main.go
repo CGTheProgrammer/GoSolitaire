a7d350ccfba8be02e2c90fca7cbb36e5c597dcd9
Cristian Gonzalez <cgtheslug@gmail.com>
2019-05-26 01:24:20 -0700

    Basic Deck structure + string array w/ supporting functions for suit and number

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
 import "fmt"
+import "strconv"
 
-func main () {
-  
+func checkSuit(suit int) string {
+	switch s := suit; s {
+	case 1:
+		//clubs
+		return "rD_"
+	case 2:
+		//diamonds
+		return "bC_"
+	case 3:
+		//hearts
+		return "rH_"
+	case 4:
+		//spades
+		return "bS_"
+	default:
+		return "error"
+	}
+}
+
+func checkRoyal(j int) string {
+	switch r := j; r {
+	case 1:
+		return "A"
+	case 11:
+		return "J"
+	case 12:
+		return "Q"
+	case 13:
+		return "K"
+	default:
+		return strconv.Itoa(j)
+	}
+}
+
+func main() {
+	var deck [53]string
+	var i int
+	var j, suit = 1, 1
+	deck[0] = "deck:\n"
+
+	for i = 1; i <= 52; i++ {
+		deck[i] = checkSuit(suit) + checkRoyal(j)
+		if j == 13 {
+			j = 1
+			suit++
+			println(suit)
+		} else {
+			j++
+		}
+	}
+
+	fmt.Println("Hello World, Let's Play Solitaire!")
+	fmt.Println(deck)
+
 }
